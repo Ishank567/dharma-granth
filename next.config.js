dcefa17c168bc9caf73ecf5eaf588479d123845d
Ishan <ishan@dharma.dev>
2026-06-01 18:56:16 +0530

    Force rebuild: explicit basePath property

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 const nextConfig = {
   output: 'export',
   distDir: 'dist',
-  basePath,
+  basePath: basePath,
   assetPrefix: basePath || undefined,
   images: {
     unoptimized: true,
